92de4849fb910b93096a7782e5761b107090da6b
Rohail Aamir <l230617@lhr.nu.edu.pk>
2026-05-01 17:13:56 +0500

    fixed firebase erros

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+// Java toolchain configuration to fix the jlink error
+// This forces Gradle to use a full JDK instead of a JRE.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
